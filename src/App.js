d307555ac46205f73438141946a3af6542773d42
lzm0010 <lzm0010@gmail.com>
2020-08-18 10:42:23 -0400

    changed App.js added pages components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+import BlogPost from './pages/BlogPost'
+import EditPost from './pages/EditPost'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hi!
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+      <BlogPost />
+      <EditPost />
+    </Fragment>
   );
 }
 
