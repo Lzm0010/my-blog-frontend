52e082f024a3d65fb8fbf698c474ba860fea5756
lzm0010 <lzm0010@gmail.com>
2020-08-30 12:08:38 -0400

    added simple markdown editor

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import ISimpleMDE from 'react-simplemde-v1';
+import 'simplemde/dist/simplemde.min.css';
+
+const option = {};
+
+const onReady = function(instance) {
+  console.log(instance.value());
+};
+
+const onEvents = {
+  'change': function() {
+    // the 'this' variable can get SimpleMDE instance
+    console.log(this.value());
+  }
+};
 
 function Editor () {
     return(
-        <div>Editor</div>
+        <ISimpleMDE 
+            option={option}
+            text={'Hi!'}
+            onReady={onReady}
+            onEvents={onEvents}
+        />
     )
 }
 
