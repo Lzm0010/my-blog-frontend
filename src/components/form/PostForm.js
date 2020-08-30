90991617035665b65cb85ed047bc7f4dcbdad63d
lzm0010 <lzm0010@gmail.com>
2020-08-30 11:58:26 -0400

    add post function

--- src/components/form/PostForm.js
+++ src/components/form/PostForm.js
@@ -5,6 +5,22 @@ function PostForm () {
     const [title, setTitle] = useState("");
     const [coverPhotoUrl, setCoverPhotoUrl] = useState("");
     const [content, setContent] = useState("");
+    const baseUrl = `http://localhost:3000`;
+
+    const addPost = () => {
+        const addPostUrl = `${baseUrl}/posts`
+        const postObj = {
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            "body": JSON.stringify({title, cover_photo_url:coverPhotoUrl, content, user_id: 1})
+        }
+        fetch(addPostUrl, postObj)
+            .then(res => res.json())
+            .then(post => console.log(post))
+    }
 
     const handleTitle = (e) => {
         const newTitle = e.target.value;
@@ -16,8 +32,13 @@ function PostForm () {
         setCoverPhotoUrl(newCoverPhoto);
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addPost();
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}> 
             <input type="text" name="title" value={title} placeholder="Add Your Title" onChange={e => handleTitle(e)} />
             <input type="text" name="coverPhoto" value={coverPhotoUrl} placeholder="Add Cover Photo Url" onChange={e => handleCoverPhoto(e)} />
             <Editor content={content}/>
