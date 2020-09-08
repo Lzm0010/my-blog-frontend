e80b10d2ba262a64fa2ce68c994ccbc5393d9946
lzm0010 <lzm0010@gmail.com>
2020-09-08 10:58:42 -0400

    added nav styling

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,14 +1,35 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import {Link} from "@reach/router";
+import styles from "./Nav.module.css";
 
 function Nav () {
     return(
-        <Fragment>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/contact">Contact</Link>
-        </Fragment>
+        <div className={styles.nav}>
+            {/* Maybe change Link to just include padded div of word
+                currently includes full invisble square
+            */}
+
+            <Link to="/">
+                <div className={styles.topleftcorner}></div>
+                <div className={styles.home} to="/">Home</div>
+            </Link>
+
+            <Link to="/about">
+                <div className={styles.toprightcorner}></div>
+                <div className={styles.about}>About</div>
+            </Link>
+
+            <Link to="/projects">
+                <div className={styles.bottomleftcorner}></div>
+                <div className={styles.projects}>Projects</div>
+            </Link>
+
+            <Link to="/contact">
+                <div className={styles.bottomrightcorner}></div>
+                <div className={styles.contact}>Contact</div>
+            </Link>
+
+        </div>
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Nav from '../components/nav/Nav';
 import Timeline from '../components/timeline/Timeline';
 import BlogPostPreviews from '../components/timeline/BlogPostPreviews';
 
+
 function Home () {
     return(
         <Fragment>
