20564d30f3b9816da087802c346b3fd7429a011e
lzm0010 <lzm0010@gmail.com>
2020-09-09 12:31:41 -0400

    added form and styling to contact page

--- src/components/basic/Header.js
+++ src/components/basic/Header.js
@@ -4,7 +4,7 @@ import styles from './Header.module.css'
 
 function Header (props) {
     return(
-        <div>
+        <div className={styles.header}>
             <h1 className={styles.title}>{props.title}</h1>
             <img src={props.img} className={styles.headerImg}/>
         </div>
--- src/components/form/ContactForm.js
+++ src/components/form/ContactForm.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styles from './ContactForm.module.css';
 
 function ContactForm () {
+    const [email, setEmail] = useState("");
+    const [content, setContent] = useState("");
+
+    const sendEmail = () => {
+
+    };
+
+    const handleEmail = (e) => {
+        const newEmail = e.target.value;
+        setEmail(newEmail);
+    };
+
+    const handleContent = (e) => {
+        const newContent = e.target.value;
+        setContent(newContent);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        sendEmail();
+    };
+
     return(
-        <div>ContactForm</div>
+        <div className={styles.container}>
+            <form onSubmit={handleSubmit}>
+                <label>Email:</label>
+                <input type="email" name="email" value={email} onChange={handleEmail} placeholder="Enter your Email here"/>
+                <textarea name="content" rows="12" cols="60" value={content} onChange={handleContent} placeholder="Tell me what's up!"/>
+                <button className={styles.btn} type="submit" value="submit">Send!</button>
+            </form>
+        </div>
     )
 }
 
