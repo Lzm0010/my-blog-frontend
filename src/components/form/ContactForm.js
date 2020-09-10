9189e27809185fe2a44246efd2470227af40d808
lzm0010 <lzm0010@gmail.com>
2020-09-10 12:38:27 -0400

    added email response

--- src/components/form/ContactForm.js
+++ src/components/form/ContactForm.js
@@ -4,28 +4,47 @@ import styles from './ContactForm.module.css';
 function ContactForm () {
     const [email, setEmail] = useState("");
     const [content, setContent] = useState("");
+    const [message, setMessage] = useState("");
+    const baseUrl = `http://localhost:3000`;
 
-    const sendEmail = () => {
+    const sendEmail = async () => {
+        const contactUrl = `${baseUrl}/contacts`;
+        const msgObj = {
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json",
+                "Accept": "application/json" 
+            },
+            "body": JSON.stringify({email, message: content})
+        }
 
+        const msgData = await fetch(contactUrl, msgObj);
+        const msg = await msgData.json();
+
+        return msg
     };
 
-    const handleEmail = (e) => {
+    const handleEmail = e => {
         const newEmail = e.target.value;
         setEmail(newEmail);
     };
 
-    const handleContent = (e) => {
+    const handleContent = e => {
         const newContent = e.target.value;
         setContent(newContent);
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async e => {
         e.preventDefault();
-        sendEmail();
+        const resp = await sendEmail();
+        setEmail("");
+        setContent("");
+        setMessage(resp.message);
     };
 
     return(
         <div className={styles.container}>
+            {message ? message : null}
             <form onSubmit={handleSubmit}>
                 <label htmlFor="email">Email</label>
                 <input type="email" name="email" value={email} onChange={handleEmail} placeholder="Enter your Email here"/>
