fef579e01101bb0e530df2121af460fa2603e838
lzm0010 <lzm0010@gmail.com>
2020-09-11 16:51:47 -0400

    added react spring for project flip

--- src/components/timeline/Project.js
+++ src/components/timeline/Project.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useSpring, animated} from 'react-spring';
 import styles from './Project.module.css';
 
 function Project ({project}) {
-    
+    const [flipped, set] = useState(false);
+    const { transform, opacity } = useSpring({
+        opacity: flipped ? 1 : 0,
+        transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+        config: { mass: 5, tension: 500, friction: 80 }
+    })
+
     const displayProjectPoints = () => {
         return project.info.map((point, i) => <li key={i}>{point}</li>)
     }
 
     return(
-        <div className={styles.projects}>
-            <h3 className={styles.title}>{project.title}</h3>
-            <img className={styles.image} src={project.img} alt={project.id}/>
-            <p className={styles.summary}>{project.summary}</p>
-            <button className={styles.btn}>
-                <a href={project.sourceCode} target="_blank" rel="noopener noreferrer">Source Code</a>
-            </button>
-            <button className={styles.btn}>
-                <a href={project.demo} target="_blank" rel="noopener noreferrer">Demo</a>
-            </button>
-            <ul className={styles.points}>
-                {displayProjectPoints()}
-            </ul>
+        <div onClick={() => set(state => !state)} className={styles.projects}>
+            <animated.div className="c" style={{ opacity: opacity.interpolate(o => 1 - o), transform } }>
+                <h3 className={styles.title}>{project.title}</h3>
+                <img className={styles.image} src={project.img} alt={project.id}/>
+                <p className={styles.summary}>{project.summary}</p>
+                <button className={styles.btn}>
+                    <a href={project.sourceCode} target="_blank" rel="noopener noreferrer">Source Code</a>
+                </button>
+                <button className={styles.btn}>
+                    <a href={project.demo} target="_blank" rel="noopener noreferrer">Demo</a>
+                </button>
+            </animated.div>
+            <animated.div class="c" style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`) }}>
+                <ul className={styles.points}>
+                    {displayProjectPoints()}
+                </ul>
+            </animated.div>
         </div>
     )
 }
--- src/components/timeline/TimeItem.js
+++ src/components/timeline/TimeItem.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Date from './Date';
+import {useSpring, animated} from 'react-spring';
 import styles from './TimeItem.module.css';
 
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
 function TimeItem (props) {
+    const [springProps, setSpringProps] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
 
     return(
         <div className={styles.timeItem}>
@@ -13,9 +18,14 @@ function TimeItem (props) {
                 <div className={styles.ball}></div>
                 <div className={styles.line}></div>
             </div>
-            <div className={styles.itemCard}>
+            <animated.div 
+                className={styles.itemCard}
+                onMouseMove={({ clientX: x, clientY: y }) => setSpringProps({ xys: calc(x, y) })}
+                onMouseLeave={() => setSpringProps({ xys: [0, 0, 1] })}
+                style={{ transform: springProps.xys.interpolate(trans) }}
+            >
                 {props.children}
-            </div>
+            </animated.div>
         </div>
     )
 }
