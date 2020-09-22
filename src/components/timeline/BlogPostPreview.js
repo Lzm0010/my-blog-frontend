cc95b6e6714e9c499d303bb7a5101e702255bf9f
lzm0010 <lzm0010@gmail.com>
2020-09-22 12:32:40 -0400

    finished styling blog preview on home page

--- src/components/timeline/BlogPostPreview.js
+++ src/components/timeline/BlogPostPreview.js
@@ -3,6 +3,9 @@ import ReactMarkdown from 'react-markdown';
 import {Link} from '@reach/router';
 import styles from './BlogPostPreview.module.css';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function BlogPostPreview ({post}) {
     const truncate = (string) => {
@@ -13,15 +16,21 @@ function BlogPostPreview ({post}) {
         <Link to={`post/${post.id}`} state={post}>
             <div>
                 <img className={styles.image} src={post.cover_photo_url} alt={post.title}/>
-                <div className={styles.title}>{post.title}</div>
+                <div className={styles.titleContainer}>
+                    <div className={styles.title}>{post.title}</div>
+                    <MoreVertIcon style={{fontSize:20}}/>
+                </div>
                 <ReactMarkdown className={styles.content} source={truncate(post.content)}/>
                 <hr />
                 <div className={styles.footContainer}>
-                    <div>{post.likes} <FavoriteBorderIcon /></div>
+                    <div><VisibilityIcon style={{fontSize: 15}}/> {post.views}</div>
                     <div>
-                        {post.comments.length !== 0 ? post.comments.length : "Write a comment"}
+                        {post.comments.length !== 0 ? ( 
+                            <span>
+                                <ChatBubbleOutlineIcon style={{fontSize:15}}/> post.comments.length
+                            </span> ) : <span className={styles.comments}>Write a comment</span>}
                     </div>
-                    <div>{post.views}</div>
+                    <div>{post.likes} <FavoriteBorderIcon style={{fontSize:15}}/></div>
                 </div>
             </div>
         </Link>
