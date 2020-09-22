0c08a0e6b7ea92389e573d49b4199d94cf66549a
lzm0010 <lzm0010@gmail.com>
2020-09-22 14:28:52 -0400

    added like functionality

--- src/components/timeline/BlogPostPreview.js
+++ src/components/timeline/BlogPostPreview.js
@@ -1,39 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactMarkdown from 'react-markdown';
 import {Link} from '@reach/router';
 import styles from './BlogPostPreview.module.css';
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import VisibilityIcon from '@material-ui/icons/Visibility';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+// import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function BlogPostPreview ({post}) {
+    const baseUrl = `http://localhost:3000`;
+    const [likes, setLikes] = useState(post.likes);
+
     const truncate = (string) => {
         return string.substring(0, 200) + "...";
     }
 
+    const editPostFave = (likes) => {
+        const editPostUrl = `${baseUrl}/posts/${post.id}`;
+        const patchObj = {
+            "method": "PATCH",
+            "headers": {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            "body": JSON.stringify({likes})
+        }
+        fetch(editPostUrl, patchObj)
+            .then(res => res.json())
+            .then(updatedPost => console.log(updatedPost));
+    }
+
+    const handleFave = () => {
+        const newLikes = likes + 1;
+        setLikes(newLikes);
+        editPostFave(newLikes);
+    }
+
     return(
-        <Link to={`post/${post.id}`} state={post}>
-            <div>
+        <div>
+            <Link to={`post/${post.id}`} state={post}>
                 <img className={styles.image} src={post.cover_photo_url} alt={post.title}/>
                 <div className={styles.titleContainer}>
                     <div className={styles.title}>{post.title}</div>
-                    <MoreVertIcon style={{fontSize:20}}/>
+                    {/* add sharing on different channels later <MoreVertIcon style={{fontSize:20}}/> */}
                 </div>
                 <ReactMarkdown className={styles.content} source={truncate(post.content)}/>
+            </Link>
                 <hr />
                 <div className={styles.footContainer}>
-                    <div><VisibilityIcon style={{fontSize: 15}}/> {post.views}</div>
-                    <div>
+                    <div><VisibilityIcon className={styles.eye} style={{fontSize: 12}}/> {post.views}</div>
+                    <Link to={`post/${post.id}`} state={post}>
                         {post.comments.length !== 0 ? ( 
                             <span>
-                                <ChatBubbleOutlineIcon style={{fontSize:15}}/> post.comments.length
+                                <ChatBubbleOutlineIcon style={{fontSize:13}}/> post.comments.length
                             </span> ) : <span className={styles.comments}>Write a comment</span>}
-                    </div>
-                    <div>{post.likes} <FavoriteBorderIcon style={{fontSize:15}}/></div>
+                    </Link>
+                        <div onClick={handleFave}>{likes} <FavoriteIcon className={styles.heart} style={{fontSize:13}}/></div>
                 </div>
-            </div>
-        </Link>
+        </div>
     )
 }
 
